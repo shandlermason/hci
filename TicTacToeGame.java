1665c8f8cb65824b36538020b201e35c73e8f458
Shandler Mason <samason4@ncsu.edu>
2025-04-03 14:03:00 -0400

    introduced bug into code

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -101,7 +101,8 @@ public class TicTacToeGame {
 
     private boolean checkRowsForWin() {
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            // Bug: Incorrect comparison of elements in the row
+            if (board[i][0] != '-' && board[i][0] == board[i][1]) { //&& board[i][1] == board[i][2]
                 return true;
             }
         }
